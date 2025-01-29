ef7f807f278c9772f0167c8d8064ba4925d6105e
latifa <154807764+latiatuw@users.noreply.github.com>
2025-01-29 19:21:51 +0300

    Update Navbar.jsx

--- frontend/travel-story-app/src/components/Navbar.jsx
+++ frontend/travel-story-app/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({ userInfo, searchQuery, setSearchQuery, onSearchNote, handleCle
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
-      <img src={LOGO} alt="logo" className="h-9" style={{width:"100px" , height:"auto"}} />
+      <img src={LOGO} alt="logo" className="h-9" style={{width:"110px" , height:"auto"}} />
 
       {isToken && (
         <>
